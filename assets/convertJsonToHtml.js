b4e8cfec920d344873ad739cf2e4b8ba245c4e14
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2018-04-13 10:07:53 -0430

    funcion para convertir json a html

---
+++ assets/convertJsonToHtml.js
@@ -0,0 +1,42 @@
+function ConvertJsonToHtml(json)    {
+    this.json=json;
+    this.convertir=convertir;
+    this.render=render;
+    this.isEmpty=isEmpty;
+    this.html = this.convertir(json);
+}
+
+function convertir(json){
+    var etiquetas="";
+    for(var i=0; i<json.length;i++){
+        var etiqueta="";
+        etiqueta="<"+json[i].element+" ";
+        if(json[i].atrib){
+            var atributos="";
+            for(j in json[i].atrib){
+                var json_temp=json[i].atrib;
+                atributos+=j+"='"+json_temp[j]+"' ";
+            }
+            etiqueta+=atributos+">";
+            if(json[i].into && json[i].into.length>0){
+                var intoHtml= convertir(json[i].into);
+                etiqueta+=intoHtml;
+                etiqueta+="</"+json[i].element+">";
+            }
+        }
+        etiquetas+=etiqueta;
+    }
+    return etiquetas;
+}
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+}
+
+function render(){
+    return this.html;
+}
